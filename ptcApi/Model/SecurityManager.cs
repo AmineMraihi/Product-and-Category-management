4b5ba969c2d996c42b650bd1c42a32912a310dc8
Unknown <amine.mraihi@esprit.tn>
2018-11-22 12:59:49 +0100

    the use of JWT from backend to frontend done.

--- ptcApi/Model/SecurityManager.cs
+++ ptcApi/Model/SecurityManager.cs
@@ -1,13 +1,24 @@
-﻿using PtcApi.Model;
+﻿using Microsoft.IdentityModel.Tokens;
+using PtcApi.Model;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ptcApi.Model
 {
     public class SecurityManager
     {
+        private JwtSettings _settings = null;
+        public SecurityManager(JwtSettings settings)
+        {
+            _settings = settings;
+        }
+
+
         public AppUserAuth ValidateUser(AppUser user)
         {
             AppUserAuth ret = new AppUserAuth();
@@ -51,6 +62,8 @@ namespace ptcApi.Model
             return list;
         }
 
+
+
         protected AppUserAuth BuildUserAuthObject(AppUser authUser)
         {
             AppUserAuth ret = new AppUserAuth();
@@ -78,7 +91,58 @@ namespace ptcApi.Model
                 {
                 }
             }
+
+            // Set JWT bearer token
+            ret.BearerToken = BuildJwtToken(ret);
+
             return ret;
+        }
+
+        protected string BuildJwtToken(AppUserAuth authUser)
+        {
+            SymmetricSecurityKey key = new SymmetricSecurityKey(
+              Encoding.UTF8.GetBytes(_settings.Key));
+
+            // Create standard JWT claims
+            List<Claim> jwtClaims = new List<Claim>();
+            jwtClaims.Add(new Claim(JwtRegisteredClaimNames.Sub,
+                authUser.UserName));
+            jwtClaims.Add(new Claim(JwtRegisteredClaimNames.Jti,
+                Guid.NewGuid().ToString()));
+
+            // Add custom claims
+            jwtClaims.Add(new Claim("isAuthenticated",
+                authUser.IsAuthenticated.ToString().ToLower()));
+
+            jwtClaims.Add(new Claim("canAccessProducts",
+                authUser.CanAccessProducts.ToString().ToLower()));
+
+            jwtClaims.Add(new Claim("canAddProduct",
+                authUser.CanAddProduct.ToString().ToLower()));
+
+            jwtClaims.Add(new Claim("canSaveProduct",
+                authUser.CanSaveProduct.ToString().ToLower()));
+
+            jwtClaims.Add(new Claim("canAccessCategories",
+                authUser.CanAccessCategories.ToString().ToLower()));
+
+            jwtClaims.Add(new Claim("canAddCategory",
+                authUser.CanAddCategory.ToString().ToLower()));
+
+            // Create the JwtSecurityToken object
+            var token = new JwtSecurityToken(
+              issuer: _settings.Issuer,
+              audience: _settings.Audience,
+              claims: jwtClaims,
+              notBefore: DateTime.UtcNow,
+              expires: DateTime.UtcNow.AddMinutes(
+                  _settings.MinutesToExpiration),
+              signingCredentials: new SigningCredentials(key,
+                          SecurityAlgorithms.HmacSha256)
+            );
+
+            // Create a string representation of the Jwt token
+            return new JwtSecurityTokenHandler().WriteToken(token); ;
         }
     }
 }
